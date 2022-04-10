4a6c98768ecbc971e3d80952dbc2dc8dcbc58472
leo-rs <90270063+leo-rs@users.noreply.github.com>
2022-04-10 13:43:13 -0500

    Working

---
+++ Pong/Pong/MainWindow.xaml.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Pong
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int ballSpeed = 15;
+        int ballSpeedX = 1;
+        int ballSpeedY = 1;
+
+        int ballX = 0;
+        int ballY = 0;
+
+        int centerX = 410;
+        int centerY = 300;
+        int botBoundary;
+
+        int playerSpeed = 10;
+        bool movingUp = false;
+        bool movingDown = false;
+
+        bool movingUpB = false;
+        bool movingDownB = false;
+        int spaceBar = 1;
+
+        int player1score = 0;
+        int player2score = 0;
+
+        DispatcherTimer timer = new DispatcherTimer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            field.Focus();
+            botBoundary = (int)(field.Height - paddle1.Height);
+
+            Canvas.SetLeft(ball, centerX - ball.Width/2);
+            Canvas.SetTop(ball, centerY-ball.Height/2);
+
+            timer.Tick += PongTimer;
+            timer.Interval = TimeSpan.FromMilliseconds(20);
+        }
+
+        private void PongTimer(object sender, EventArgs e)
+        {
+
+            ball.Visibility = Visibility.Visible;
+
+            ballX = (int)Canvas.GetLeft(ball);
+            ballY = (int)Canvas.GetTop(ball);
+
+            ballX += ballSpeed * ballSpeedX;
+            ballY += ballSpeed * ballSpeedY;
+
+            Canvas.SetLeft(ball, ballX);
+            Canvas.SetTop(ball, ballY);
+
+            if (movingDown && Canvas.GetTop(paddle1) < botBoundary)
+            {
+                int n = (int)Canvas.GetTop(paddle1);
+                Canvas.SetTop(paddle1, n + playerSpeed);
+            }
+            if (movingUp && Canvas.GetTop(paddle1) > 0)
+            {
+                int n = (int)Canvas.GetTop(paddle1);
+                Canvas.SetTop(paddle1, n - playerSpeed);
+            }
+
+            if (movingDownB && Canvas.GetTop(paddle2) < botBoundary)
+            {
+                int n = (int)Canvas.GetTop(paddle2);
+                Canvas.SetTop(paddle2, n + playerSpeed);
+            }
+            if (movingUpB && Canvas.GetTop(paddle2) > 0)
+            {
+                int n = (int)Canvas.GetTop(paddle2);
+                Canvas.SetTop(paddle2, n - playerSpeed);
+            }
+
+            if (Canvas.GetLeft(ball) <= Canvas.GetLeft(paddle1) + paddle1.Width && (Canvas.GetTop(ball) >= Canvas.GetTop(paddle1) - ball.Height && Canvas.GetTop(ball) <= Canvas.GetTop(paddle1) + paddle1.Height + ball.Height))
+            {
+                ballSpeedX *= -1;
+            }
+
+            if (Canvas.GetLeft(ball) + ball.Width >= Canvas.GetLeft(paddle2) && (Canvas.GetTop(ball) >= Canvas.GetTop(paddle2) - ball.Height && Canvas.GetTop(ball) <= Canvas.GetTop(paddle2) + paddle2.Height + ball.Height))
+            {
+                ballSpeedX *= -1;
+            }
+
+            if (Canvas.GetLeft(ball) < 0)
+            {
+                player2score += 1;
+                p2score.Text = "" + player2score;
+                ResetField();
+            }
+
+            if (Canvas.GetLeft(ball) > field.Width)
+            {
+                player1score += 1;
+                p1score.Text = "" + player1score;
+                ResetField();
+            }
+
+            if (Canvas.GetTop(ball) <= 0 || Canvas.GetTop(ball) + ball.Height >= field.Height)
+            {
+                ballSpeedY *= -1;
+            }
+
+        }
+
+
+        private void ResetField() 
+        {
+            status.Visibility = Visibility.Visible;
+            ball.Visibility = Visibility.Hidden;
+
+            timer.Stop();
+            ballSpeed *= -1;
+            Random random = new Random();
+            int newPositionX = random.Next(300, 400);
+            int newPositionY = random.Next(100,300);
+
+            Canvas.SetLeft(ball, newPositionX);
+            Canvas.SetTop(ball, newPositionY);
+
+            spaceBar++;
+        }
+
+        private void win_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Z)
+            {
+                movingDown = true;
+            }
+            if (e.Key == Key.A)
+            { 
+                movingUp = true;
+            }
+              
+            if (e.Key == Key.M)
+            {
+                movingDownB = true;
+            }
+            if (e.Key == Key.K)
+            {
+                movingUpB = true;
+            }
+
+            if (e.Key == Key.Space)
+            {
+                spaceBar++;
+                if (spaceBar % 2 == 1)
+                {
+                    status.Visibility = Visibility.Visible;
+                    timer.Stop();
+                }
+                else
+                {
+                    status.Visibility = Visibility.Hidden;
+                    timer.Start();
+                }
+            }
+        }
+
+        private void win_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Z)
+            {
+                movingDown = false;
+            }
+            if (e.Key == Key.A)
+            {
+                movingUp = false;
+            }
+
+            if (e.Key == Key.M)
+            {
+                movingDownB = false;
+            }
+            if (e.Key == Key.K)
+            {
+                movingUpB = false;
+            }
+        }
+
+
+    }
+}
